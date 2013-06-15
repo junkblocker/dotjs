3905b3e5399f6b574f56f6c6a7e6d52c3d9a3069
Manpreet Singh <junkblocker@yahoo.com>
2013-06-15 11:30:41 -0700

    Catch and log uncaught .js exceptions

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -2,7 +2,11 @@ $.ajax({
   url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
   dataType: 'text',
   success: function(d){
+    try {
     $(function(){ eval(d) })
+    } catch (e) {
+        console.log("Uncaught exception caught by dotjs.js", e);
+    }
   },
   error: function(){
     console.log('no dotjs server found at localhost:3131')
