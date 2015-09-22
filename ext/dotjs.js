d6f031fabd3a10e978ae23a5a476516dda940451
Manpreet Singh <junkblocker@yahoo.com>
2015-09-21 23:13:42 -0700

    Remove jQuery. Make IPv6 compatible. Creates certs ourselves.

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,14 +1,17 @@
-$.ajax({
-  url: 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js',
-  dataType: 'text',
-  success: function(d){
-    try {
-    $(function(){ eval(d) })
-    } catch (e) {
-        console.log("Uncaught exception caught by dotjs.js", e);
-    }
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function (event) {
+  documentReady(this.responseText);
+}
+
+xhr.open('get', 'https://localhost:3131/'+location.hostname.replace(/^www\./,'')+'.js', true);
+xhr.responseType = 'text';
+xhr.send();
+
+function documentReady (responseType) {
+  if (!document.body) {
+    setTimeout(documentReady, 10, responseText);
+  } else {
+    eval(responseText);
   }
-})
+}
